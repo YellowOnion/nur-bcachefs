7460004ba95dac2b399958d416886485d4bbe44a
GithubAction <action@gluo.nz>
2022-09-23 12:36:09 +0000

    Automated update

--- pkgs/bcachefs-kernel/default.nix
+++ pkgs/bcachefs-kernel/default.nix
@@ -3,8 +3,8 @@
 with lib;
 
 let
-  commit = "4fb76991f7742ead11ab1d8e0bb7542f08d1bbed";
-  diffHash = "1ghzl0dx1lvxay4vr9qya2xnvavw44ypwa4wj4srw5vc65zlwbhr";
+  commit = "1194fb84803e3cd442d0872c396720d50b4fcd81";
+  diffHash = "1knj6v603vjanmnkilbbacc5lz4sfygcgnc9qqxp5g1cds35q3ks";
   shorthash = lib.strings.substring 0 7 commit;
   kernelVersion = kernel.version;
   oldPatches = kernelPatches;
